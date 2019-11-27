f2c3f38299e5c55bc55f22c662c1b0b6f1593c1f
long.bai <long198519@gmail.com>
2019-11-27 17:23:27 +0100

    remove die

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -34,9 +34,7 @@ class DefaultController extends Controller
     	$eventDispatcher = new EventDispatcher();
     	$eventDispatcher->dispatch(	'testevent.testevent', $event
     			);
-    	// [...]
-    	echo 'test';
-    	var_dump($event);die();
+    	
     	return $this->render('default/index.html.twig', [
     			'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
     	]);
